0018640db0ede1d6b99051a8923114747b332555
Leo Casagrande <leo@greenberry.io>
2018-08-21 16:42:37 +0200

    setup web server

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var toReturn  = [{
+	"firstname" : 'leo',
+	"lastname" : 'casagrande',
+	"skills" : 'javascript'
+
+},{
+	"firstname" : 'jc',
+	"lastname" : 'baudier',
+	"skills" : 'php'
+
+}];
+
+// je déclare mes fichiers statiques
+app.use('/js', express.static('./client/js'));
+app.use('/css', express.static('./client/css'));
+
+app.get('/', function (req, res) {
+   res.sendFile(__dirname + '/client/index.html')
+});
+
+app.get('/liste', function (req, res) {
+   res.json(toReturn);
+});
+
+app.listen(9999, function () {
+	  console.log('serveur lancé');
+});
+
+
+
